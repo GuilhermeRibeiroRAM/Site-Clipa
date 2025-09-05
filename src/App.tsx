d479f3df856cfe5b33c1cb37bc58fdc87a0a4637
GuilhermeRibeiroRAM <guilherme.ribeiro@cepedi.org.br>
2025-09-05 16:33:44 -0300

    #04 - Aplicação das rotas para teste de funcionamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./Pages/Login"
+
 export default function App() {
 
   return (
     <>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
